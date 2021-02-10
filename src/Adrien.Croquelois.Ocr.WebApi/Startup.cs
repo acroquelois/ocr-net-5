6ea7f8d250ab6b9e4f09c88d72f4cf8f5cccae9b
Croquelois <croquelois.adrien@gmail.com>
2021-02-10 10:47:59 +0100

    Add swagger

--- src/Adrien.Croquelois.Ocr.WebApi/Startup.cs
+++ src/Adrien.Croquelois.Ocr.WebApi/Startup.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Adrien.Croquelois.Ocr.WebApi
 {
@@ -18,6 +14,10 @@ namespace Adrien.Croquelois.Ocr.WebApi
         {
             services.AddControllers();
             services.AddScoped<Ocr>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,11 +29,12 @@ namespace Adrien.Croquelois.Ocr.WebApi
             }
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
         }
     }
 }
